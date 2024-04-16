67ba851730a98d560f240ee6c204e7c68c072476
lntv0407 <thoang0303@gmail.com>
2024-04-16 19:12:21 -0400

    update the next

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: "https://group-11-server.cyclic.app",
+    NEXT_PUBLIC_API_URL: "https://a3-server.cyclic.app/",
     APP_ID: "3c4c2acf",
     APP_KEY: "0d155571d9ecdeb8c6295a839f9bb6a7",
   },
